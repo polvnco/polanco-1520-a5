ee0271b4c976937c81574731c031a0755ad668bc
polo <cpolanco000@gmail.com>
2021-07-20 23:34:10 -0400

    the inventory is editable, can add and remove to the inventory, also implemented a way for it to open a file (still needs to actually save and load a file and the real work)

--- src/main/java/ucf/assignments/inventoryGUI.java
+++ src/main/java/ucf/assignments/inventoryGUI.java
@@ -4,10 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -17,6 +21,12 @@ public class inventoryGUI extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Inventory.fxml")));
+            FileChooser fileChooser = new FileChooser();
+            Button button = new Button("Select File");
+            button.setOnAction(e -> {
+                File selectedFile = fileChooser.showOpenDialog(primaryStage);
+            });
+
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Inventory Tracker");
